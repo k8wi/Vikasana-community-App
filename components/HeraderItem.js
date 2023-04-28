b303a40e5d811fa953ac25e595485374906ba6fe
k8wi <thushar.r02@gmail.com>
2023-04-28 20:27:46 +0530

    header 2

--- components/HeraderItem.js
+++ components/HeraderItem.js
@@ -2,8 +2,8 @@
 
 function HeaderItem({Icon,title}) {
     return (
-      <div className="flex flex-col items-center group">
-       <Icon className='h-8 mb-2'/>
+      <div className="flex flex-col items-center group cursor-pointer w-12 sm:w-8 hover:text-white ">
+       <Icon className="h-8 mb-2 group-hover:animate-bounce"/>
          <p className=" opacity-0 group-hover:opacity-100 tracking-widest ">
           {title}
          </p>
